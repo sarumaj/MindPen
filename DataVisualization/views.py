6e0c93db6e8161eb623231a9e317438d6da4ad2b
slim <slimmoj@gmail.com>
2023-09-04 12:59:30 +0900

    Barchart x-axis

--- DataVisualization/views.py
+++ DataVisualization/views.py
@@ -139,10 +139,10 @@ def data(request):
     df_previous = pd.DataFrame(dic_previous_data)
     barchart = px.bar(
         df_previous,
-        # x="Date",
-        # y="Average",
+        x="Date",
+        y="Average",
         title="Previous Month Averages",
-        labels={"x": "Date", "y": "Averages"}
+        # labels={"x": "Date", "y": "Averages"}
     )
     barchart.update_xaxes(
         type='category',
