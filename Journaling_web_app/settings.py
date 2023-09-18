c797bd804664d9052ad88c16408345779a5a3d49
slim <slimmoj@gmail.com>
2023-09-18 15:06:22 +0900

    GEBUG=False

--- Journaling_web_app/settings.py
+++ Journaling_web_app/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "a5958fb3723b2e77787ed08bec37d9c0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ["*"]
 ALLOWED_HOSTS = ["penmind.onrender.com", "localhost", "127.0.0.1"]
