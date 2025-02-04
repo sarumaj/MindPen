42348a23aca58930dd358fa310ffbdbc61cb4d66
slim <slimmoj@gmail.com>
2025-02-04 15:43:34 +0900

    Time zone is set to UTC

--- Journaling_web_app/settings.py
+++ Journaling_web_app/settings.py
@@ -119,11 +119,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Seoul'
-
+TIME_ZONE = 'UTC'
 USE_I18N = True
-
 USE_TZ = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
